c732636ab42fcd5f304e6c081c53c39c2813a89e
Petro Verkhogliad <vpetro@gmail.com>
2013-04-27 17:53:20 -0400

    Remove redundant brackets and $

--- CommonsGame.hs
+++ CommonsGame.hs
@@ -29,7 +29,7 @@ data GameCard = Red
 
 -- some test values here
 currentTable = [(0, 100), (40, 102), (40, 102), (40,102), (44, 106), (46, 108), (48, 110), (50, 0)] :: GameTable
-currentPlay = (M.fromList [(Red,3),(Green,3),(Black,2)]) :: Map GameCard Int
+currentPlay = M.fromList [(Red,3),(Green,3),(Black,2)] :: Map GameCard Int
 currentState = (0, 100) :: GameState
 numPlayers = 8 :: Int
 
@@ -41,29 +41,29 @@ score Black played table
     | otherwise = 0.0
 
 score Green played table
-    | (M.member Green played) && (M.member Black played) = -20.0
-    | (M.member Green played) = fromIntegral $ snd $ _rewardFromNumPlayed played table
+    | M.member Green played && M.member Black played = -20.0
+    | M.member Green played = fromIntegral $ snd $ _rewardFromNumPlayed played table
     | otherwise = 0.0
 
 score Red played table
-    | (M.member Red played) && (M.member Orange played) = _orangeBenefit + _partialRedScore
-    | (M.member Red played) = _partialRedScore
+    | M.member Red played && M.member Orange played = _orangeBenefit + _partialRedScore
+    | M.member Red played = _partialRedScore
     | otherwise = 0.0
     where
         _partialRedScore = fromIntegral $ fst $ _rewardFromNumPlayed played table
         _orangeBenefit = 10.0 * (fromIntegral $ played ! Orange)
 
 score Yellow played table
-    | (M.member Yellow played) = 6.0
+    | M.member Yellow played = 6.0
     | otherwise = 0.0
 
 score Orange played table
-    | (M.member Orange played) = -8.0 / (fromIntegral $ played ! Orange)
+    | M.member Orange played = -8.0 / (fromIntegral $ played ! Orange)
     | otherwise = 0.0
 
 _updateMarker :: GamePlay -> Int -> Int -> Int
 _updateMarker played turn m
-    | (mod turn 6 == 0) = m - gc + 0 {-- the zero should be a random value --}
+    | mod turn 6 == 0 = m - gc + 0 {-- the zero should be a random value --}
     | otherwise = m - gc
     where
         gc
